ca2e48973cd303eea0f9684458150c34a88f3d95
docker <dock3r@gmail.com>
2016-11-03 10:31:56 +0100

    Ugly workaround for swipebox mouse events

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,36 @@
 (function($) {
+	var hidebarsdelay = 3000;
+	function setTimeout () {
+		if ( hidebarsdelay > 0 ) {
+			var $this = this;
+			window.clearTimeout( $this.timeout );
+			$this.timeout = window.setTimeout( function() {
+				var bars = $( '#swipebox-top-bar, #swipebox-bottom-bar' );
+				bars.removeClass( 'visible-bars' );
+			},
+			hidebarsdelay
+			);
+		}
+	}
+
 	$('.swipebox-album').click(function(e) {
 		e.preventDefault();
+
+
 		var images_data = $(this).data('images');
 		var images_data_arr = $.map(images_data, function(value, index) {
 			return [value];
 		});
-		$.swipebox(images_data_arr);
+		console.log(hidebarsdelay);
+		var options = {hideBarsDelay: hidebarsdelay};
+		$.swipebox(images_data_arr, options);
+
+		$('#swipebox-slider').mousemove( function() {
+			var bars = $( '#swipebox-top-bar, #swipebox-bottom-bar' );
+			bars.addClass( 'visible-bars' );
+			setTimeout();
+		});
+
+
 	});
 })(jQuery);
